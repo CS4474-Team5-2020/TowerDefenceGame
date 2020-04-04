36c4585f9073687954d12fc37ddb06b62955575c
Daniel Le <daniel.le36@hotmail.com>
2020-04-04 12:21:27 -0400

    Added countdown for Next Wave button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
     public ObjectPool Pool { get; set; }
     //Keeps track of the current time in the wave
     //TODO: Update number next to "Next Wave" button with time remaining to indicate how much gold they will get for pressing it
-    public int Time { get; set; }
+
+    //Constant for the default next wave score bonus/remaining wave time
+    private const int Countdown = 50;
+    //Remaining time left in wave
+    public int WaveTime { get; set; }
+    //Next Wave Text object to reference
+    private Text NextButtonTxt { get; set; }
+    
     private void Awake()
     {
         Pool = GetComponent<ObjectPool>();
@@ -14,7 +22,12 @@ public class GameManager : Singleton<GameManager>
     // Use this for initialization
     void Start()
     {
-
+        //Set up Next Wave button text
+        WaveTime = Countdown;
+        NextButtonTxt = GameObject.Find("NextWaveBtn").GetComponentInChildren<Button>().GetComponentInChildren<Text>();
+        NextButtonTxt.text = "Next Wave +" + WaveTime;
+        //Update remaining time every second
+        InvokeRepeating("UpdateTime", 1f, 1f);
     }
 
     // Update is called once per frame
@@ -23,6 +36,18 @@ public class GameManager : Singleton<GameManager>
 
     }
 
+    void UpdateTime()
+    {
+        WaveTime--;
+        //Reset wave time and spawn next wave at the end of the wave time
+        if( WaveTime < 0)
+        {
+            WaveTime = Countdown;
+            StartWave();
+        }
+        //Update Next Wave button text
+        NextButtonTxt.text = "Next Wave +" + WaveTime;
+    }
     public void StartWave()
     {
         StartCoroutine(SpawnWave());
@@ -33,8 +58,10 @@ public class GameManager : Singleton<GameManager>
         //TODO: Have txt file with the composition of each wave (enemy type(s) and number of enemies)
         //TODO: Get current wave number and spawn corresponding enemies for wave
         //TODO: Spawn enemies in certain sections of map
+        //Reset Next Wave Text
+        WaveTime = Countdown;
+        NextButtonTxt.text = "Next Wave +" + WaveTime;
         //TODO: Increase gold based on how much time left in current wave
-        
         string type = "Enemy";
         Pool.GetObject(type);
         yield return new WaitForSeconds(2.5f);
