574d345350a4d2e17d464972ce4729a5fa2f1eba
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2020-04-04 16:52:56 -0400

    Added money manager system, dispaying money balance on bill and value of kill on coin

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    private int moneyBalance = 0;
+    private int moneyValue = 0;  //Amount of money gained per kill
+    public Text moneyBalanceText;
+    public Text moneyValueText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Load in the saved money balance just before the current game session begins
+        this.moneyBalance = PlayerPrefs.GetInt("moneyBalance");
+        this.moneyBalanceText.text = this.moneyBalance.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void saveMoneyBalance() {
+        PlayerPrefs.SetInt("moneyBalance", this.moneyBalance);
+        PlayerPrefs.Save();
+    }
+
+    public int getMoneyBalance() {
+        return this.moneyBalance;
+    }
+
+    public int getMoneyValue() {
+        return this.moneyValue;
+    }
+
+    public void setMoneyBalance(int value) {
+        //Set money balance and counter values
+        this.moneyBalance += value;
+        this.moneyValue = value;
+
+        //Set UI Text Canvas Objects with money balance and counter values
+        this.moneyBalanceText.text = this.moneyBalance.ToString();
+        this.moneyValueText.text = this.moneyValue.ToString();
+
+        //Save the current balance into Playerprefs so it can be saved for later game sessions
+        this.saveMoneyBalance();
+    }
+}
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,12 +9,20 @@ public class EnemyAI : MonoBehaviour
 {
     public GameObject finishZone;
     [SerializeField] private int health;
-    
+
+    private MoneyManager money;
+    private int value = 2;   //Equivalent to how much money player gets when killing this enemy - might want to set this in inspector instead?
+    private bool isBalanceIncreased = false;    //Balanced increased?
 
     // Start is called before the first frame update
     void Start()
     {
-
+        try {
+            this.money = GameObject.FindObjectOfType<MoneyManager>();
+        }
+        catch(Exception e) {
+            Debug.LogException(e);
+        } 
     }
 
     // Update is called once per frame
@@ -32,6 +41,12 @@ public class EnemyAI : MonoBehaviour
     private void Die()
     {
         gameObject.SetActive(false);
+
+        //If game object is not active and if balance hasn't been increased already, then increase money balance
+        if (!gameObject.activeSelf && !this.isBalanceIncreased) {
+             this.isBalanceIncreased = true;
+             money.setMoneyBalance(value);   //When enemy dies, increase money balance
+        }
     }
 
     public bool IsAlive()
