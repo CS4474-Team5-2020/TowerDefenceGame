44816aed255cc84afae338781ae2af4c68287ae0
mmanka1 <33776177+mmanka1@users.noreply.github.com>
2020-04-06 03:53:18 -0400

    Added tooltip over money balance

---
+++ Assets/OnUIHover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnUIHover : MonoBehaviour
+{
+    public GameObject balance;
+
+    public void MouseOver() {
+        balance.SetActive(true);
+    }
+
+    public void MouseOut() {
+        balance.SetActive(false);
+    }
+}
